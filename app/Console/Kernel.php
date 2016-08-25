1bfe27ecffe6460520ab1323e92844c00dc3874e
vagrant <vagrant@homestead>
2016-08-25 15:41:23 +0000

    2nd test on deploy test in server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('seed:Databaseseed')->monthlyOn(29, '15:00');
-//        $schedule->command('seed:Databaseseed')->everyMinute();
+//        $schedule->command('seed:Databaseseed')->monthlyOn(29, '15:00');
+        $schedule->command('seed:Databaseseed')->everyMinute();
     }
 }
