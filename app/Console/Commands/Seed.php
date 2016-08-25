c0c3b8d0b786627ffd6751d26a13eb98acc03b30
vagrant <vagrant@homestead>
2016-08-25 15:38:14 +0000

    seed test in server

--- app/Console/Commands/Seed.php
+++ app/Console/Commands/Seed.php
@@ -38,37 +38,37 @@ class Seed extends Command
      */
     public function handle()
     {
-            DB::table('clients')
-                        ->update(['paidStatus' => 'due']);
+//            DB::table('clients')
+//                        ->update(['paidStatus' => 'due']);
 
-//        DB::table('clients')->insert([
-//            'name' => str_random(10),
-//            'email' => str_random(10).'@gmail.com',
-//            'password' => bcrypt('secret'),
-//            'status' => 'free',
-//            'gender' => 'female',
-//
-////            'area' => str_random(10),
-//            'area' => 'kafrul',
-//            'username' => str_random(7),
-//
-//            'Father' => str_random(6),
-//            'Mother' => str_random(6),
-//            'Company' => str_random(8),
-//            'gender' => 'female',
-//            'dob' => \Carbon\Carbon::now(),
-//
-//
-//            'PermanentAddress' => str_random(17),
-//            'PresentAddress' => str_random(17),
-//            'phone' => random_int(10,11),
-//            'connectedFrom' => \Carbon\Carbon::now(),
-//            'bill' => random_int(500,2000),
-//            'dataScheme' => random_int(1,10),
-//            'payment' => random_int(1,31),
-//            'comment' => str_random(200),
-//            'paidStatus' => 'paid',
-//        ]);
+        DB::table('clients')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'status' => 'free',
+            'gender' => 'female',
+
+//            'area' => str_random(10),
+            'area' => 'kafrul',
+            'username' => str_random(7),
+
+            'Father' => str_random(6),
+            'Mother' => str_random(6),
+            'Company' => str_random(8),
+            'gender' => 'female',
+            'dob' => \Carbon\Carbon::now(),
+
+
+            'PermanentAddress' => str_random(17),
+            'PresentAddress' => str_random(17),
+            'phone' => random_int(10,11),
+            'connectedFrom' => \Carbon\Carbon::now(),
+            'bill' => random_int(500,2000),
+            'dataScheme' => random_int(1,10),
+            'payment' => random_int(1,31),
+            'comment' => str_random(200),
+            'paidStatus' => 'paid',
+        ]);
         $this->info('succesfully');
     }
 }
